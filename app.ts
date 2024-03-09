816949c476f79ef4ad1f91435e45ae48f90a1724
viewthiti <65011212173@msu.ac.th>
2024-03-10 02:37:40 +0700

    oo

--- app.ts
+++ app.ts
@@ -12,8 +12,8 @@ export const app = express();
 // });
 app.use(
   cors({
-    // origin: "*",
-    origin: "http://localhost:4200",
+    origin: "*",
+    // origin: "http://localhost:4200",
   })  
 );
 
